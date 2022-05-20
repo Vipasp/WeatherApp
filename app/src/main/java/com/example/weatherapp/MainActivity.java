5b25aaeb645b0eba0e01f72642969ac73a9677e7
oleg <vipasp@yandex.ru>
2022-05-20 14:00:32 +0300

    My Weather with comments

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -1,46 +1,53 @@
 package com.example.weatherapp;
+/*
+ Приложение Погода
+ показывает температуру и облачность в указанном пользователем
+ городе или по индексу
+*/
 
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity; //AppCompatActivity применяется для обратной совместимости в плане дизайна
 
+import android.os.AsyncTask;//механизм для перемещения трудоёмких операций в фоновый пото
+import android.os.Bundle; //оболочка над коллекцией ArrayMap для создания более комфортного в работе контейнера
+import android.view.View; //View – компонент макета, который пользователь может видеть и взаимодействовать с ним
+import android.widget.EditText; //текстовое поле для пользовательского ввода
+import android.widget.TextView; //отображения текста без возможности редактировани
+
+import org.json.JSONObject; //JavaScript Object Notation обмен данными с сервером
 import org.json.JSONException;
-import org.json.JSONObject;
+import java.io.BufferedInputStream; //для упрощения чтения текста из двоичных входных потоков
+import java.io.BufferedReader; //читает текст из потока ввода символов
+import java.io.IOException; //Обработчик исключений
+import java.io.InputStream; //интерфейс потока чтения
+import java.io.InputStreamReader; //мост, позволяющий преобразовать byte stream в character stream
+import java.net.MalformedURLException; //проверяет правильность адреса URL
+import java.net.URL; //Обработка URL (унифицированный указатель ресурсов)
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.HttpsURLConnection; //забираем JSON по SSL
 
 public class MainActivity extends AppCompatActivity {
-    private final String APPID = "4a2180cc176960d45b97c3b3912454c8";
+    private final String APPID = "4a2180cc176960d45b97c3b3912454c8"; //API ключ получаем после регистрации в openweathermap.org
     private final String WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?q=%s&lang=ru&units=metric&APPID=" + APPID;
-    private TextView textViewWeather;
-    private EditText editTextCity;
+    //синтаксис берем из документации разработчика openweathermap
+    // Забанили РФ, с 20.05.2022 нужен VPN!
+    private TextView textViewWeather; //объявляем наши поля для отображения погоды
+    private EditText editTextCity; //город или индекс
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextCity = findViewById(R.id.editTextCity);
-        textViewWeather = findViewById(R.id.textViewWeather);
+        editTextCity = findViewById(R.id.editTextCity); //R есть Java-класс, автоматически создается из наших ресурсов в процессе сборки
+        textViewWeather = findViewById(R.id.textViewWeather); //findViewById метод, который находит представление по заданному идентификатору
     }
 
     public void onClickShowWeather(View view) {
         String city = editTextCity.getText().toString().trim();
-        if (!city.isEmpty()) {
+        if (!city.isEmpty()) { //если получили ответ и город найден
             DownloadWeatherTask task = new DownloadWeatherTask();
-            String url = String.format(WEATHER_URL, city);
-            task.execute(url);
+            String url = String.format(WEATHER_URL, city); //формируем строку
+            task.execute(url); //запускаем загрузчик
         }
     }
 
@@ -48,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected String doInBackground(String... strings) {
+            //ответ ждем в фоновом режиме
             URL url = null;
             HttpsURLConnection urlConnection = null;
             StringBuffer result = new StringBuffer();
@@ -57,7 +65,7 @@ public class MainActivity extends AppCompatActivity {
                 InputStream inputStream = urlConnection.getInputStream();
                 InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
                 BufferedReader reader = new BufferedReader(inputStreamReader);
-                String line = reader.readLine();
+                String line = reader.readLine(); //пытаемся получить ответ
                 while (line != null) {
                     result.append(line);
                     line = reader.readLine();
@@ -69,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             } finally {
                 if (urlConnection != null) {
-                    urlConnection.disconnect();
+                    urlConnection.disconnect(); //в конце нужно закрыть соединение
                 }
             }
             return null;
@@ -78,16 +86,16 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected void onPostExecute(String s) {
             if (s.isEmpty()) {
-                textViewWeather.setText("Город не найден");
+                textViewWeather.setText("Город не найден"); //Если ответ отличается от нашего формата, разбирать дальше не будем
             } else {
                 super.onPostExecute(s);
                 try {
                     JSONObject jsonObject = new JSONObject(s);
-                    String city = jsonObject.getString("name");
+                    String city = jsonObject.getString("name"); //разбираем на JSON по тегам
                     String temp = jsonObject.getJSONObject("main").getString("temp");
                     String description = jsonObject.getJSONArray("weather").getJSONObject(0).getString("description");
-                    String weather = String.format("%s\nтемпература: %s\n %s\n", city, temp, description);
-                    textViewWeather.setText(weather);
+                    String weather = String.format("%s\nтемпература: %s\n %s\n", city, temp, description); //формируем строку с ответом
+                    textViewWeather.setText(weather); //выводим
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
